1a39cc235db3fb8d7560ed33daed3b7ff4f85b84
Brandon <git@bsquid.io>
2019-04-23 15:30:24 -0700

    getid command and test command
    getid must be run in the bots chat, nowhere else.

--- cogs/basic.py
+++ cogs/basic.py
@@ -18,6 +18,17 @@ class Basic(commands.AutoCog):
     async def hello_command(self, ctx):
         await ctx.send(f'Hello {ctx.author.name}!')
 
+    
+    @commands.command(name='getid')
+    async def getid_command(self, ctx):
+        if ctx.channel.name == self.bot.nick:
+            await ctx.send(f'{ctx.author.name}, your ID is {ctx.author.id}!')
+
+
+    @commands.command(name='test')
+    async def test_command(self, ctx):
+        print(ctx.message.author.tags)
+
 
 def prepare(bot):
     # Module is being loaded
--- cogs/utils/checks.py
+++ cogs/utils/checks.py
@@ -2,3 +2,6 @@ import os
 
 def is_owner(ctx):
     return ctx.message.author.id == int(os.environ['OWNER_ID'])
+
+def is_mod(ctx):
+    return ctx.message.author.is_mod == 1
